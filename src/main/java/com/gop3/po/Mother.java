1599acf06fddb274f5bfbfd04b4089312ab50036
drak <1614852697@qq.com>
2020-01-14 21:52:03 +0800

    drgn 4th upload~~

--- src/main/java/com/gop3/po/Mother.java
+++ src/main/java/com/gop3/po/Mother.java
@@ -10,17 +10,17 @@ import java.util.List;
  */
 @Data
 public class Mother {
-    Integer id;//妈妈id
-    String name;//妈妈姓名
-    String wx_openid;//微信id
-    String phone;//电话号码
-    String address;//地址
-    String birthday;//宝宝出生日期
-    Integer pregnantWeeks;//怀孕周数
-    Integer babyWeeks;//宝宝出生周数
-    String icon;//妈妈头像url
-    Date createTime;//妈妈用户创建时间
-    Integer state;//妈妈用户状态，0表示正常、1表示禁用
+    private Integer id;//妈妈id
+    private String name;//妈妈姓名
+    private String wx_openid;//微信id
+    private String phone;//电话号码
+    private String address;//地址
+    private String birthday;//宝宝出生日期
+    private Integer pregnantWeeks;//怀孕周数
+    private Integer babyWeeks;//宝宝出生周数
+    private String icon;//妈妈头像url
+    private Date createTime;//妈妈用户创建时间
+    private Integer state;//妈妈用户状态，0表示正常、1表示禁用
 
     private List<Doctor> doctorList;//关联医生表，多对多
 }
